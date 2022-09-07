b3d527e77e83a8ff40c8ed2bafe81b57e26f911e
Sung Kim <ksungha66@gmail.com>
2022-09-07 13:38:21 -0700

    Udemy Section 2 Basic CRUD operations

---
+++ BulkyBook/BulkyBookWeb/Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+    }
+}
