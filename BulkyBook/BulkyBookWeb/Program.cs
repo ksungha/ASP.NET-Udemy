4173115f310d45801ed18c24bc2e75d95b540aa4
Sung Kim <ksungha66@gmail.com>
2022-09-07 19:04:56 -0700

    Add boostwatch css file for styling

--- BulkyBook/BulkyBookWeb/Program.cs
+++ BulkyBook/BulkyBookWeb/Program.cs
@@ -5,11 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
